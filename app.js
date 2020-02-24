f2e82b41160fe51f758c05a05097a3dd59408d83
Sakub <sandomierzj@gmail.com>
2020-02-24 09:40:46 +0100

    Display tasks dynamically

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ Vue.component('tasks-wrapper', {
     template: `
     
         <div class="tasksWrapper">
-            <div class="task">
-                <p class="task__title">Some task</p>
+            <div class="task" v-for="task in listOfTasks">
+                <p class="task__title">{{task.name}}</p>
                 <button class="task__doneButton"><i class="fas fa-check doneButton__icon"></i></button>
             </div>
             <div class="app__inputWrapper">
@@ -11,7 +11,14 @@ Vue.component('tasks-wrapper', {
             <button class="app__addButton"><i class="fa fa-plus addButton__icon"></i></button>
         </div>
         </div>
-    `
+    `,
+    data(){
+        return{
+            listOfTasks: [
+                {name: 'some task', completed: false, id: Math.random()},
+            ]
+        }
+    }
 
 })
 var app = new Vue({
