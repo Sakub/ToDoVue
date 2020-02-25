c8c23fd6406ad9d9e3d2a4b392f65cd44012a09f
Sakub <sandomierzj@gmail.com>
2020-02-25 10:00:17 +0100

    Add availability to add tasks

--- app.js
+++ app.js
@@ -2,23 +2,40 @@ Vue.component('tasks-wrapper', {
     template: `
     
         <div class="tasksWrapper">
-            <div class="task" v-for="task in listOfTasks">
+            <div class="task" v-for="task in listOfTasks" :key="task.id">
                 <p class="task__title">{{task.name}}</p>
                 <button class="task__doneButton"><i class="fas fa-check doneButton__icon"></i></button>
             </div>
             <div class="app__inputWrapper">
-            <input type="text" class="app__input" placeholder="Add task">
-            <button class="app__addButton"><i class="fa fa-plus addButton__icon"></i></button>
+            <input type="text" class="app__input" placeholder="Add task" v-model="taskName">
+            <button class="app__addButton" @click="test"><i class="fa fa-plus addButton__icon"></i></button>
         </div>
         </div>
     `,
     data() {
         return {
-            listOfTasks: [{
-                name: 'some task',
+            taskName:null,
+            listOfTasks: [
+                {
+                    name: 'some task',
+                    completed: true,
+                    id: Math.random()
+                },
+                {
+                    name: 'some task 2',
+                    completed: false,
+                    id: Math.random()
+                }
+            ]
+        }
+    },
+    methods:{
+        test(){
+            this.listOfTasks.push({
+                name: this.taskName,
                 completed: false,
                 id: Math.random()
-            }, ]
+            })
         }
     }
 })
